7f7d99970884af4a2af02f7c7f8517a3444536c8
Bruno Nascimento <wintergamesk@gmail.com>
2025-08-29 15:03:12 -0300

    first commit

---
+++ Course100/Entities/Order.cs
@@ -0,0 +1,67 @@
+﻿using Course100.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course100.Entities
+{
+	internal class Order
+	{
+		public DateTime Moment { get; set; }
+		public OrderStatus Status { get; set; }
+		public List<OrderItem> Items { get; set; } = [];
+		public Client Client { get; set; }
+
+		public Order() { }
+
+		public Order(DateTime moment, OrderStatus status, Client client)
+		{
+			Moment = moment;
+			Status = status;
+			Client = client;
+		}
+
+		public void AddItem(OrderItem item)
+		{
+			Items.Add(item);
+		}
+
+		public void RemoveItem(OrderItem item)
+		{
+			Items.Remove(item);
+		}
+
+		public double Total()
+		{
+			double sum = 0.0;
+			foreach(OrderItem item in Items)
+			{
+				sum += item.SubTotal();
+			}
+			return sum ;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder stringBuilder = new();
+			stringBuilder.AppendLine("ORDER SUMMARY:");
+			stringBuilder.Append("Order moment:");
+			stringBuilder.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+			stringBuilder.Append("Order status:");
+			stringBuilder.AppendLine(Status.ToString());
+			stringBuilder.Append("Client:");
+			stringBuilder.Append(Client.Name + " (" + Client.BirthDate.ToString("dd/MM/yyy") + ")");
+			stringBuilder.Append(" - " + Client.Email);
+			stringBuilder.AppendLine("Order items: ");
+
+			foreach(OrderItem orderItem in Items)
+			{
+				stringBuilder.AppendLine(orderItem.ToString());
+			}			
+
+			return stringBuilder.ToString();
+		}
+	}
+}
---
+++ Course100/Program.cs
@@ -0,0 +1,46 @@
+﻿using Course100.Entities;
+using Course100.Entities.Enums;
+using System.Globalization;
+
+Console.WriteLine("Enter cliente data: ");
+Console.Write("Name: ");
+string? name = Console.ReadLine();
+
+Console.Write("Email: ");
+string? email = Console.ReadLine();
+
+Console.Write("Birth Date (DD/MM/YYYY): ");
+DateTime birthDate = (DateTime.ParseExact(Console.ReadLine() ?? "0", "dd/MM/yyyy", CultureInfo.InvariantCulture));
+Console.WriteLine();
+
+Client client = new(name, email, birthDate);
+
+Console.WriteLine("Enter order data: ");
+Console.Write("Status: ");
+string? status = Console.ReadLine();
+OrderStatus orderStatus = Enum.Parse<OrderStatus>(status ?? "PendingPayment");
+
+Console.Write("How many items to this order? ");
+var itemsToOrder = int.Parse(Console.ReadLine() ?? "0");
+
+Order order = new(DateTime.Now, orderStatus, client);
+
+
+for (int i = 1; i <= itemsToOrder; i++)
+{
+	Console.WriteLine($"Enter #{i} item data: ");
+	Console.Write("Product name: ");
+	var productName = Console.ReadLine();
+
+	Console.Write("Product price: ");
+	var productPrice = double.Parse(Console.ReadLine() ?? "0");
+
+	Console.Write("Quantity: ");
+	var quantity = int.Parse(Console.ReadLine() ?? "0");
+
+	Product product = new(productName, productPrice);
+	OrderItem orderItem = new(quantity, productPrice, product);
+	order.AddItem(orderItem);
+}
+
+Console.WriteLine(order);
